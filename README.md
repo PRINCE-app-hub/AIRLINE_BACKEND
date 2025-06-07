64870b4d35fb05e96b27440df63e146a22f7e083
Prince_P <princekumar486140@gmail.com>
2025-06-07 17:27:13 +0000

    Added sequelizer and mysql

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # AIRLINE_BACKEND
+# Adding Sequalizer npm , Sequlizer cli , and MYSQL2 to connect the very two to MYSQL as it helps bind our data 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const bodyparser=require('body-parser'); 
 const setupandStartServer = async () => {
     const app = express();
     const {PORT}=require('./config/serverConfig')
-
+    app.use(bodyparser.json);
+    app.use(bodyparser.urlencoded({extended:true}));
     app.listen(PORT, () => {
         console.log(`Server started at port ${PORT}`);
     });
